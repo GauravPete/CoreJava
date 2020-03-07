bac14b972fac75993fc06f1c02d358fa5712b579
GauravPete <gauravpete.gp@gmail.com>
2020-03-07 14:46:06 +0530

    initialised domy commit

---
+++ src/com/basic/programs/AreaOfRectangle.java
@@ -0,0 +1,26 @@
+package com.basic.programs;
+
+/*
+ Program to calculate the area of rectangle
+            or
+ Program to find the area of parallelogram            
+ 
+ formula Area of Rectangle = Height * width
+*/
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		double height = 25.00;
+		double width = 10.00;
+		
+		double areaOfRectangle;
+		areaOfRectangle = height * width;
+		
+		System.out.println("Area of Rectangle is : "+areaOfRectangle);
+				
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/basic/programs/VolumeOfSquere.java
@@ -0,0 +1,10 @@
+package com.basic.programs;
+
+public class VolumeOfSquere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
